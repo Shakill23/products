4cb8a90834e475f5230971371ef005756f143144
Shakeel <shakeeldvd23@gmail.com>
2024-08-01 16:15:53 +0200

    Server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// comment
+import express from 'express'
+import path from 'path'
+import {config} from 'dotenv'
+config()
+
+const app = express()
+const port = +process.env.PORT || 2000
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve('./static/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).sendFile(path.resolve('./static/about.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+})
